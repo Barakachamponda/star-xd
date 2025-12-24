c098b752c10ffe076b0aa519c7d4128380fb4a2f
Barakachamponda <159643438+Barakachamponda@users.noreply.github.com>
2025-12-24 19:36:18 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,10 @@
 //=====💫 sᴛᴀʀ xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
-  SESSION_ID: '', // use sessin id if inbuilt pairing not working
-  MODE: "public", // public or private
+  BOT_NAME: 'FRIDAY', // ur bot name
+  OWNER_NAME: 'EL CHAPO', //your name
+  OWNER_NUMBER: '255796825074', // add your phone number
+  SESSION_ID: 'starcore~', // use sessin id if inbuilt pairing not working
+  MODE: "private", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
 };
